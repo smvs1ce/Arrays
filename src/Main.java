efb739038579f12294f79ea3fa4db4e9c8456807
John Hairston <j.hairston71@gmail.com>
2022-07-11 18:43:15 -0400

    array sum

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[] = {5,6,77,9,0,5,654,34,4,3,3};
+	    int sum=0;
+		for(int i=0;i<arr.length;i++){
+		    sum+=arr[i];
+		}
+		System.out.println(sum);
+	}
+}
